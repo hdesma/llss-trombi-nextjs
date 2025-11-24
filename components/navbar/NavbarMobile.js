4a1f5223a50db9bd2a7d55fcc6d996a1b3485279
hdesma <hugues.desmarest@gmail.com>
2025-11-24 15:58:19 +0100

    Gestion de onblur dans navbar mobile

--- components/navbar/NavbarMobile.js
+++ components/navbar/NavbarMobile.js
@@ -1,24 +1,28 @@
 'use client'
 import Image from "next/image"
 import NavLink from "./NavLink"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import classes from "./NavBarMobile.module.css"
+import { useRouter } from "next/navigation"
 
 export default function NavbarMobile({ organes }) {
-    const [isDisplayed, setIsDisplayed] = useState(true)
-    const imagePath = isDisplayed ? "/images/default/burger-menu-open.png" : "/images/default/burger-menu-close.png"
+    const [isDisplayed, setIsDisplayed] = useState(false)
+    const imagePath = isDisplayed ? "/images/default/burger-menu-close.png" : "/images/default/burger-menu-open.png";
+    const routeur = useRouter()
+
     function toggleMenu() {
         setIsDisplayed(!isDisplayed)
     }
 
-    function handleLoseFocus() {
-        if (!isDisplayed) {
-            setIsDisplayed(true)
+    function handleOnBlur(event) {
+        if (!event.currentTarget.contains(event.relatedTarget)) {
+            setIsDisplayed(false)
         }
     }
-    return <nav className={classes.navMobile} onBlur={handleLoseFocus}>
+
+    return <nav className={classes.navMobile} onBlur={(event) => handleOnBlur(event)}>
         <button onClick={toggleMenu}><Image src={imagePath} height="20" width="20" alt="burger menu" /></button>
-        {!isDisplayed && <ul>
+        {isDisplayed && <ul>
             <NavLink id="accueil" href="/">Accueil</NavLink>
             {organes.map((organe) => {
                 return <NavLink href={organe.alias} id={organe.id} key={organe.id}>{organe.short}</NavLink>
