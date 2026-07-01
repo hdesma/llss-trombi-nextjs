c356f5c8f9a6067de182c933eb1edf0cc2c34f65
hdesma <hugues.desmarest@gmail.com>
2026-07-01 20:07:56 +0200

    Refacto des scripts de set up de la DB, et stop tracking fichier config (pour pathing, chaque env a le sien perso en attendant mieux)

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  basePath: "/LLSS/llss-trombi-nextjs",
-  assetPrefix: "/LLSS/llss-trombi-nextjs",
-};
-
-export default nextConfig;
